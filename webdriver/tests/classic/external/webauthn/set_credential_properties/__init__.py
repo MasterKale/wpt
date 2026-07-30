c5e8332a8fe6df1102a90976f6613a302dbac597
Matthew Miller <mattmil3@cisco.com>
2026-07-30 14:39:45 -0700

    Make it easier to partially set cred props

--- webdriver/tests/classic/external/webauthn/set_credential_properties/__init__.py
+++ webdriver/tests/classic/external/webauthn/set_credential_properties/__init__.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, asdict
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 
 @dataclass
@@ -13,11 +13,34 @@ def set_credential_properties(
     session: Any,
     authenticator_id: str,
     credential_id: str,
-    props: SetCredentialPropertiesProps,
+    props: Union[SetCredentialPropertiesProps, dict],
 ):
+    """
+    This method calls the Set Credential Properties endpoint as per the WebAuthn spec:
+
+    https://w3c.github.io/webauthn/#sctn-automation-set-credential-properties
+
+    `props` supports an instance of `SetCredentialPropertiesProps`, but also accepts a raw `dict`
+    value. The latter allows for quick-and-dirty passing in of partially populated properties:
+
+    ```
+    props = SetCredentialPropertiesProps(
+        backupEligibility=True,
+        backupState=True,
+        signCount=0,
+    )
+
+    props = asdict(props)
+    del props["signCount"]  # Don't send signCount at all
+
+    set_credential_properties(..., props=props)
+    ```
+    """
+    if isinstance(props, SetCredentialPropertiesProps):
+        props = asdict(props)
+
     return session.transport.send(
         "POST",
         f"/session/{session.session_id}/webauthn/authenticator/{authenticator_id}/credentials/{credential_id}/props",
-        asdict(props),
+        props,
     )
-
--- webdriver/tests/classic/external/webauthn/set_credential_properties/set.py
+++ webdriver/tests/classic/external/webauthn/set_credential_properties/set.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from . import set_credential_properties, SetCredentialPropertiesProps
+from . import set_credential_properties, SetCredentialPropertiesProps, asdict
 from .. import create_credential
 
 
@@ -37,3 +37,31 @@ def test_set_credential_properties(session: Any, authenticator: str):
     assert updated_credential["backupEligibility"] == True
     assert updated_credential["backupState"] == True
     assert updated_credential["signCount"] == 42
+
+def test_set_credential_properties_omit_sign_count(session: Any, authenticator: str):
+    credential_id = "Y3JlZDI"
+
+    # Add a new credential
+    new_credential = create_credential(credential_id=credential_id, sign_count=1)
+    session.web_authn.add_credential(authenticator, new_credential)
+
+    [new_credential] = session.web_authn.get_credentials(authenticator)
+
+    # Establish a baseline of assertions so we know what values we're updating
+    assert new_credential["signCount"] == 1
+
+    # Update the credential, omitting `signCount`
+    _props = SetCredentialPropertiesProps(
+        backupEligibility=True,
+        backupState=True,
+        signCount=None,
+    )
+    _props = asdict(_props)
+    del _props["signCount"]
+
+    set_credential_properties(session, authenticator, credential_id, _props)
+
+    # Assert the existing credential's `signCount` is unchanged
+    [updated_credential] = session.web_authn.get_credentials(authenticator)
+
+    assert updated_credential["signCount"] == 1
